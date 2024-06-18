cc8d67df1a07b9839e27f5d1746299991723e6f1
Sujal509 <82167616+Sujal509@users.noreply.github.com>
2024-06-18 16:24:56 +0530

    Updates Installation process

--- install_dependencies.ps1
+++ install_dependencies.ps1
@@ -1,4 +1,8 @@
-Write-Host "Installing Node modules..." 
-npm install 
-Write-Host "Installing Python requests library..." 
+Write-Host "Creating directory for puppeteer"
+New-Item -Name browser_files -ItemType Directory
+
+Write-Host "Installing Node Modules..."
+npm install
+
+Write-Host "Installing Python requests library..."
 pip install requests
